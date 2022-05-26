ccd4b92b0fc16a26c40e11bf88b2481ff8446ae5
Jeremy <jeremy__francis@hotmail.com>
2022-05-25 18:58:30 -0700

    added fonts and a bit more text in javascript

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
 const h2 = document.createElement("h2");
 h2.textContent = "Where I hope to be"
 document.querySelector("body").appendChild(h2);
+const p = document.createElement("p")
+p.textContent = "I want to learn and get the basics of coding down so I can move into the field and continue to learn and grow my knowledge. I would like to one day work at a vehicle manufacturer and write the code for the computers which control the various sub systems on the vehicle. I would also be very interested in working for a company that designs programs and solutions for other businesses, anything really that would be varied and allow me to grow and develop my skills"
+document.querySelector('body').appendChild(p)
